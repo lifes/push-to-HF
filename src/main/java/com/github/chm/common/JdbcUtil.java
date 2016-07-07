bf9e09508fae0aa76ba359ed3096ff942b922994
peter <330199814@qq.com>
2016-07-08 03:45:07 +0800

    s

--- src/main/java/com/github/chm/common/JdbcUtil.java
+++ src/main/java/com/github/chm/common/JdbcUtil.java
@@ -3,7 +3,6 @@ package com.github.chm.common;
 import com.github.chm.exception.InitDataConnectionPoolException;
 import com.jolbox.bonecp.BoneCP;
 import com.jolbox.bonecp.BoneCPConfig;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -12,7 +11,6 @@ import java.sql.SQLException;
  */
 public class JdbcUtil {
     private static volatile BoneCP connectionPool;
-
     public static synchronized void initConnectionPool(String jdbcurl, String username, String password) throws InitDataConnectionPoolException {
         shutDownConnectionPool();
         connectionPool = null;
@@ -26,7 +24,7 @@ public class JdbcUtil {
         try {
             connectionPool = new BoneCP(config);
         } catch (SQLException e) {
-            throw new InitDataConnectionPoolException("初始化数据库连接池失败!!!",e);
+            throw new InitDataConnectionPoolException("初始化数据库连接池失败!!!(可能由于地址或用户名或密码不对,或有问题数据库服务异常引起)",e);
         }
     }
 
@@ -37,8 +35,17 @@ public class JdbcUtil {
     }
     public static Connection getConnection() throws SQLException {
         if(connectionPool == null){
-            throw new RuntimeException("数据库连接池未初始化或初始化失败");//程序应该保证不会出现这种情况
+            throw new RuntimeException("数据库连接池未初始化或初始化失败(可能由于地址或用户名或密码不对,或有问题数据库服务异常引起)");//程序应该保证不会出现这种情况
         }
         return connectionPool.getConnection();
+    }
+    public static String testDataConnectionStatus(){
+        if(connectionPool == null){
+            return "数据库连接池未初始化,或初始化异常(可能由于地址或用户名或密码不对,或有问题数据库服务异常引起)";
+        }
+        if(connectionPool.getDbIsDown().get()){
+            return "数据库状态: 挂掉了";
+        }
+        return "数据库状态: 正常";
     }
 }
