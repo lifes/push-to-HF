6f43732fdd8af3adcd09069b91da4fbca5d80967
peter <330199814@qq.com>
2016-07-08 05:32:30 +0800

    wtf

---
+++ src/main/java/com/github/chm/common/HttpRequest.java
@@ -0,0 +1,77 @@
+package com.github.chm.common;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+
+/**
+ * Created by chenhuaming on 16/7/8.
+ */
+public class HttpRequest {
+    private boolean hasInit = false;
+
+    //连接超时时间，默认10秒
+    private int socketTimeout = 10000;
+
+    //传输超时时间，默认30秒
+    private int connectTimeout = 30000;
+
+    //请求器的配置
+    private RequestConfig requestConfig;
+
+    //HTTP请求器
+    private CloseableHttpClient httpClient;
+
+    public HttpRequest() throws UnrecoverableKeyException, KeyManagementException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        init();
+    }
+    private void init() throws NoSuchAlgorithmException {
+        SSLContext sslcontext = SSLContext.getDefault();
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                sslcontext,
+                new String[]{"TLSv1"},
+                null,
+                SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+        httpClient = HttpClients.custom()
+                .setSSLSocketFactory(sslsf)
+                .build();
+
+        requestConfig = RequestConfig.custom().setSocketTimeout(socketTimeout).setConnectTimeout(connectTimeout).build();
+
+
+    }
+    public String get(String url) throws IOException {
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.setConfig(requestConfig);
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            return EntityUtils.toString(entity, "UTF-8");
+        }finally {
+            response.close();
+        }
+    }
+    public String get2(String url){
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setEntity("");
+        return null;
+    }
+    public static void main(String args[]) throws UnrecoverableKeyException, IOException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+       String s = new HttpRequest().get("http://www.hikvision.com/cn/index.html?jmode=j1");
+        System.out.println();
+    }
+}
